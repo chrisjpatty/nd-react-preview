43ce2315752c7a9f7015228b818daf7028dbf008
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2016-11-09 20:16:45 -0700

    Added Initial Implementation
    Added basic style setup and dummy data. Added ability to update input
    values with React function.

---
+++ nd-react-preview/js/app.jsx
@@ -0,0 +1,185 @@
+﻿window.App = React.createClass({
+    componentWillMount: function () {
+        console.log("Initial Data: ", this.props.data);
+        this.setState({
+            data: this.props.data
+        })
+    },
+    getInitialState: function(){
+        return{
+            activeView: 1
+        }
+    },
+    getViewData: function(){
+        var data = this.props.data;
+        var found = null;
+        var activeView = this.state.activeView;
+        data.filter(function (item) {
+            if (item.itemId == activeView) {
+                found = item;
+            }
+        })
+        return found;
+    },
+    setView: function (id) {
+        var data = this.props.data;
+        data = data.filter(function (item) {
+            if (item.itemId == id) {
+                item.active = true;
+            } else {
+                item.active = false;
+            }
+            return item;
+        })
+
+        this.setState({
+            data: data,
+            activeView: id
+        })
+    },
+    setField: function (fieldId, value, sectionId) {
+        //console.log(fieldId, sectionId, this.state.activeView, value);
+        var activeView = this.state.activeView;
+        var data = this.props.data;
+        data = data.filter(function (item) {
+            if (item.itemId == activeView) {
+                var sections = item.sections.filter(function (section) {
+                    if (section.sectionId == sectionId) {
+                        var fields = section.fields.filter(function (field) {
+                            if (field.fieldId == fieldId) {
+                                field.value = value;
+                            }
+                            return field;
+                        })
+                        section.fields = fields;
+                    }
+                    return section;
+                })
+                item.sections = sections;
+            }
+            return item;
+        })
+        this.setState({
+            data: data
+        })
+    },
+    render: function () {
+        var capture = this;
+        return(
+            <div className="app-container">
+                <div className="app-sidebar">
+                    {
+                        this.props.data.map(function (item, i) {
+                            return (<SidebarItem id={item.itemId} key={i} setView={capture.setView} viewLabel={item.viewLabel} active={item.active ? "active" : "" }/>);
+                        })
+                    }
+                </div>
+                <div className="app-body">
+                    {
+                        this.state.activeView != null ?
+                        <AppBody data={this.getViewData()} setField={this.setField} />
+                        :
+                        null
+                    }
+                </div>
+            </div>
+        )
+    }
+})
+
+var SidebarItem = React.createClass({
+    setView: function () {
+        this.props.setView(this.props.id);
+    },
+    render: function () {
+        return(
+            <div className="sidebar-item row" onClick={this.setView}>
+                <div className="col-xs-12">
+                    <div className={"item center-block " + this.props.active} >{this.props.viewLabel}</div>
+                </div>
+            </div>    
+        )
+    }
+})
+
+var AppBody = React.createClass({
+    setField: function (fieldId, value, sectionId) {
+        this.props.setField(fieldId, value, sectionId);
+    },
+    render: function () {
+        var capture = this;
+        return(
+            <div>
+                {
+                    this.props.data.sections.map(function (section, i) {
+                        return <BodySection title={section.title} sectionId={section.sectionId} fields={section.fields} type={section.type} setField={capture.setField} key={i} />;
+                    })
+                }
+            </div>
+        )
+    }
+})
+
+var BodySection = React.createClass({
+    setField: function(fieldId, value){
+        this.props.setField(fieldId, value, this.props.sectionId);
+    },
+    render: function () {
+        var capture = this;
+        return(
+            <div className="body-section">
+                <h4 className="section-title">{this.props.title}</h4>
+                {
+                    this.props.fields.map(function (field, i) {
+                        return <Field field={field} key={i} fieldId={field.fieldId} setField={capture.setField} width={field.sizeOverride ? field.width : "inherit"} clear={field.clear} options={field.options}/>
+                    })
+                }
+            </div>    
+        )
+    }
+})
+
+var Field = React.createClass({
+    setField: function (event) {
+        this.props.setField(this.props.fieldId, event.target.value);
+    },
+    getField: function(){
+        var field = this.props.field;
+        var style = {
+            width: this.props.width,
+            clear: this.props.clear
+        }
+        switch (field.type) {
+            case "text":
+                return (
+                    <div className="body-field-wrapper body-text-field" style={style}>
+                        <label className="text-label" >{field.label}</label>
+                        <input type="text" onChange={this.setField} value={field.value}/>
+                    </div>
+                )
+                break;
+            case "select":
+                return (
+                    <div className="body-field-wrapper body-select-field" style={style}>
+                        <label className="text-label">{field.label}</label>
+                        <select>
+                            {
+                                field.options.map(function (option,i) {
+                                    return <option key={i}>{option}</option>
+                                })
+                            }
+                        </select>
+                    </div>    
+                )
+        }
+    },
+    render: function () {
+        return(
+            <div>
+                {
+                    this.getField()
+                }
+            </div>    
+        )
+    }
+})
---
+++ nd-react-preview/js/data.js
@@ -0,0 +1,179 @@
+﻿var dummyData = [
+    {
+        itemId: 1,
+        viewLabel: "Contact",
+        active: true,
+        sections: [
+            {
+                title: "Contact Name",
+                type: "custom",
+                sectionId: 1,
+                fields: [
+                    {
+                        fieldId: 1,
+                        type: "text",
+                        label: "First Name",
+                        sizeOverride: true,
+                        width: "50%",
+                        clear: "both",
+                        placeholder: "John",
+                        value: ""
+                    },
+                    {
+                        fieldId: 2,
+                        type: "text",
+                        label: "Last Name",
+                        sizeOverride: true,
+                        width: "50%",
+                        clear: "both",
+                        placeholder: "Doe",
+                        value: ""
+                    }
+                ]
+            },
+            {
+                title: "Home Address",
+                type: "custom",
+                sectionId: 2,
+                fields: [
+                    {
+                        fieldId: 1,
+                        type: "text",
+                        label: "Street Address",
+                        sizeOverride: true,
+                        width: "70%",
+                        clear: "both",
+                        placeholder: "346 West St.",
+                        value: ""
+                    },
+                    {
+                        fieldId: 2,
+                        type: "text",
+                        label: "City",
+                        sizeOverride: true,
+                        width: "40%",
+                        clear: "left",
+                        placeholder: "Smithfield",
+                        value: ""
+                    },
+                    {
+                        fieldId: 3,
+                        type: "text",
+                        label: "State",
+                        sizeOverride: true,
+                        width: "20%",
+                        clear: "right",
+                        placeholder: "ND",
+                        value: ""
+                    },
+                    {
+                        fieldId: 4,
+                        type: "text",
+                        label: "Postal Code",
+                        sizeOverride: true,
+                        width: "30%",
+                        clear: "both",
+                        placeholder: "123456",
+                        value: ""
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        itemId: 2,
+        viewLabel: "Business Information",
+        active: false,
+        sections: [
+            {
+                title: "Basic Information",
+                type: "custom",
+                sectionId: 1,
+                fields: [
+                    {
+                        fieldId: 1,
+                        type: "select",
+                        label: "Business Type",
+                        sizeOverride: true,
+                        width: "60%",
+                        clear: "both",
+                        placeholder: "John",
+                        value: "",
+                        options: [
+                            "Domestic Limited Liability Corporation",
+                            "Foreign Limited Liability Corporation",
+                            "Sole Proprietorship"
+                        ]
+                    },
+                    {
+                        fieldId: 2,
+                        type: "text",
+                        label: "Business Name",
+                        sizeOverride: true,
+                        width: "50%",
+                        clear: "both",
+                        placeholder: "Doe",
+                        value: ""
+                    },
+                    {
+                        fieldId: 3,
+                        type: "text",
+                        label: "Confirm Name",
+                        sizeOverride: true,
+                        width: "50%",
+                        clear: "both",
+                        placeholder: "Doe",
+                        value: ""
+                    }
+                ]
+            },
+            {
+                title: "Agent Address",
+                type: "custom",
+                sectionId: 2,
+                fields: [
+                    {
+                        fieldId: 1,
+                        type: "text",
+                        label: "Street Address",
+                        sizeOverride: true,
+                        width: "70%",
+                        clear: "both",
+                        placeholder: "346 West St.",
+                        value: ""
+                    },
+                    {
+                        fieldId: 2,
+                        type: "text",
+                        label: "City",
+                        sizeOverride: true,
+                        width: "40%",
+                        clear: "left",
+                        placeholder: "Smithfield",
+                        value: ""
+                    },
+                    {
+                        fieldId: 3,
+                        type: "text",
+                        label: "State",
+                        sizeOverride: true,
+                        width: "20%",
+                        clear: "right",
+                        placeholder: "ND",
+                        value: ""
+                    },
+                    {
+                        fieldId: 4,
+                        type: "text",
+                        label: "Postal Code",
+                        sizeOverride: true,
+                        width: "30%",
+                        clear: "both",
+                        placeholder: "123456",
+                        value: ""
+                    }
+                ]
+            }
+        ]
+    }
+]
